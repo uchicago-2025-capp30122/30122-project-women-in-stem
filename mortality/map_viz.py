a74c4bcb3f7483d16e2136d598ec5a66aec5c70d
Elizabeth Ronan <eronan@uchicago.edu>
2025-02-23 22:08:03 -0600

    Last edits on map for now

--- mortality/map_viz.py
+++ mortality/map_viz.py
@@ -1,6 +1,13 @@
 import pandas as pd
 from pathlib import Path
 import plotly.express as px
+from dash import Dash, dcc, html, dash_table
+
+# # From this data source, we aim to use the summary statistics provided by the 
+# # second green map. This data lives in a CSV file, composed of 51 rows, 
+# # representing all 50 states and the District of Columbia, and 5 columns. 
+# # We plan to use the column labelled as "0–18.6" which refers to the maternal 
+# # mortality rates per 100,000 live births.
 
 def load_data():
     """
@@ -13,47 +20,32 @@ def load_data():
 
     df = pd.read_csv(file)
     df = df[df['state'] != 'district of columbia']
+    df = df[['state', 'abbrev', 'deaths', 'lower', 'upper']]
+    df = df.sort_values(by=['lower'], ascending=False)
+    df.columns = ['State', 'Abbreviation', 'Deaths', 'Lower Estimate', 'Upper Estimate']
 
     return df
 
 def map_mortalities():
+    """
+    Creates a chloropleth map by state of maternal mortalities
+    """
     df = load_data()
-    fig = px.choropleth(locations= 'abbrev', locationmode="USA-states", scope="usa",
-                        color = 'lower', hover_data= 'state', 
-                        color_continuous_scale=px.colors.sequential.Blues,
-                        title='Deaths per 100,000 female population by state',
+
+    fig = px.choropleth(locations= 'Abbreviation', locationmode="USA-states", scope="usa",
+                        color = 'Deaths', hover_data= 'State',
+                        color_discrete_sequence = ["#084594", "#4292c6", "#9ecae1", "#c6dbef"],
+                        title = 'Deaths per 100,000 female population by state',
                         data_frame=df)
-    fig.show()
+    app = Dash()
+    app.layout = html.Div([
+        dcc.Graph(figure=fig),
+        dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+    ])
+
+    app.run_server(debug=True, use_reloader=False)
     
-map_mortalities()
+## Want to add option to display different demographics -- switch to merged data
 
-
-# def load_shapefile():
-#     """
-#     Loads in a shapefile of US states
-#     """
-#     file = "data/shapefiles/tl_2024_us_state/ts_2024_us_state.shp"
-#     shp_path = Path(__file__).parent.parent.joinpath(file)
-
-#     states = {}
-
-#     with shapefile.Reader(shp_path) as sf:
-#         for shape_rec in sf.shapeRecords():
-#             # shape_rec.shape.points - list of WKT points, used to construct
-#             #                             a shapely.Polygon
-#             # We are assuming Polygon here! If this file had points/lines/etc.
-#             # changes would be needed.
-#             polygon = Polygon(shape_rec.shape.points)
-#             # shape_rec.record contains the attributes as a list or dictionary
-#             # in this file record[0] contains the neighborhood name
-#             name = shape_rec.record[0]
-#             # associate polygon with name
-#             states[name] = polygon
-            
-#     return states
-
-# # From this data source, we aim to use the summary statistics provided by the 
-# # second green map. This data lives in a CSV file, composed of 51 rows, 
-# # representing all 50 states and the District of Columbia, and 5 columns. 
-# # We plan to use the column labelled as "0–18.6" which refers to the maternal 
-# # mortality rates per 100,000 live births.
+if __name__ == '__main__':
+    map_mortalities()
