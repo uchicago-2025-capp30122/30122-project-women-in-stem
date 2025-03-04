58335e36567e52df1c4b19a780605057a274ae32
Elizabeth Ronan <eronan@uchicago.edu>
2025-03-04 11:27:59 -0600

    Working on map

--- mortality/map_viz.py
+++ mortality/map_viz.py
@@ -9,20 +9,26 @@ from dash import Dash, dcc, html, dash_table
 # # We plan to use the column labelled as "0–18.6" which refers to the maternal 
 # # mortality rates per 100,000 live births.
 
-def load_data():
+DEATHS = Path(__file__).parent.parent.joinpath("data/scrape_data/deaths.csv")
+ABORTION_LAWS = Path(__file__).parent.parent.joinpath("data/scrape_data/abortion.csv")
+
+def load_data(file, abortion):
     """
     Loads in the example CSV for visualizations
 
     Inputs: none
     Outputs: Pandas dataframe
     """
-    file = Path(__file__).parent.parent.joinpath("data/scrape_data/deaths.csv")
 
     df = pd.read_csv(file)
-    df = df[df['state'] != 'district of columbia']
-    df = df[['state', 'abbrev', 'deaths', 'lower', 'upper']]
-    df = df.sort_values(by=['lower'], ascending=False)
-    df.columns = ['State', 'Abbreviation', 'Deaths', 'Lower Estimate', 'Upper Estimate']
+    
+    if abortion:
+        df = df[['Location', 'Statutory Limit on Abortions']]
+    
+    else:
+        df = df[['state', 'abbrev', 'deaths', 'lower', 'upper']]
+        df = df.sort_values(by=['lower'], ascending=False)
+        df.columns = ['State', 'Abbreviation', 'Deaths', 'Lower Estimate', 'Upper Estimate']
 
     return df
 
@@ -30,22 +36,41 @@ def map_mortalities():
     """
     Creates a chloropleth map by state of maternal mortalities
     """
-    df = load_data()
+    df = load_data(DEATHS, False)
 
     fig = px.choropleth(locations= 'Abbreviation', locationmode="USA-states", scope="usa",
                         color = 'Deaths', hover_data= 'State',
                         color_discrete_sequence = ["#084594", "#4292c6", "#9ecae1", "#c6dbef"],
                         title = 'Deaths per 100,000 female population by state',
                         data_frame=df)
-    app = Dash()
-    app.layout = html.Div([
-        dcc.Graph(figure=fig),
-        dash_table.DataTable(data=df.to_dict('records'), page_size=10)
-    ])
-
-    app.run_server(debug=True, use_reloader=False)
     
-## Want to add option to display different demographics -- switch to merged data
+    return fig
+
+def map_abortion_laws():
+    df = load_data(ABORTION_LAWS, True)
+
+    #fig = px.choropleth(locations = )
+
+def run_app():
+    """ 
+    Using dash, display maps
+    """
+    mortality_map = map_mortalities()
+    mortality_data = load_data(DEATHS, False)
+
+    abortion_data = load_data(ABORTION_LAWS, True)
+    print(abortion_data)
+
+    #mortality_map.show()
+
+    # app = Dash()
+    # app.layout = html.Div([
+    #     dcc.Graph(figure=mortality_map),
+    #     dash_table.DataTable(data=mortality_data.to_dict('records'), page_size=10)
+    # ])
+
+    # app.run_server(debug=True, use_reloader=False)
+    
 
 if __name__ == '__main__':
-    map_mortalities()
+    run_app()
