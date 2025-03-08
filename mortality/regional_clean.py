d4c88f08a5e5754e409893fede6772f8287bd514
Madelin De Jesus <ms.jiggy@Mss-MacBook-Pro.local>
2025-03-08 17:55:13 -0600

    divided rate by 100, removed lower

--- mortality/regional_clean.py
+++ mortality/regional_clean.py
@@ -4,7 +4,7 @@ from pathlib import Path
 #Cleaning Regional data
 region_path_open = Path(__file__).parent.parent.joinpath("data/downloaded_data/region_age_educ_race.csv")
 region_path_write = Path(__file__).parent.parent.joinpath("data/clean_reg_age_educ.csv")
-fieldnames = ['region', 'race', 'education', 'ten_year_age_groups', 'deaths', 'percent_total_deaths']
+fieldnames = ['region', 'race', 'education', 'ten_year_age_groups', 'deaths', 'mortality_rate', 'mortality_binary']
 
 
 # Read the CSV file
@@ -17,13 +17,21 @@ with open(region_path_write, 'w', newline='', encoding='utf-8') as csv_file:
     writer.writeheader()
     
     for row in content:
+        mortality_rate = (float(row['% of Total Deaths'].strip().lower()[:3]))
+        
+        if mortality_rate > 0.0: 
+            mortality_binary = 1
+        else: 
+            mortality_binary = int(0)
+
         cleaned_row = {
-            'region': row['Census Region'].strip().lower()[17:],
-            'race': row['Single Race 6'].strip().lower(),
-            'education': row['Education'].strip().lower().replace('?', "'") if row['Education'] != 'Not Available' else 'unknown',
+            'region': row['Census Region'].strip().upper()[17]+ row['Census Region'].strip().lower()[18:],
+            'race': row['Single Race 6'].strip(),
+            'education': row['Education'].strip().replace('?', "'").replace(".", "") if row['Education'] != 'Not Available' else 'unknown',
             'ten_year_age_groups': row['Ten-Year Age Groups Code'].strip().lower(),
             'deaths': row['Deaths'].strip().lower(),
-            'percent_total_deaths': float(row['% of Total Deaths'].strip().lower()[:3])
+            'mortality_rate': round(mortality_rate/100, 3),
+            'mortality_binary' : mortality_binary
         }
         writer.writerow(cleaned_row)
-        
+
