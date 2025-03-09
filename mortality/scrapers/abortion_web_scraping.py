d9096a568194ea1b6988d06ce11d859cee023bc6
Alexandrea Harriott <aharriott@uchicago.edu>
2025-03-09 12:07:13 -0500

    updating scrapers

--- mortality/scrapers/abortion_web_scraping.py
+++ mortality/scrapers/abortion_web_scraping.py
@@ -1,4 +1,3 @@
-import re
 import json
 import csv
 import pathlib
@@ -8,64 +7,73 @@ from mortality.utils import STATE_ABBREVIATIONS
 
 BASE_DIR = pathlib.Path(__file__).parent.parent.parent
 
-# https://www.kff.org/womens-health-policy/state-indicator/gestational-limit-abortions/?currentTimeframe=0&selectedRows=%7B%22states%22:%7B%22all%22:%7B%7D%7D,%22wrapups%22:%7B%22united-states%22:%7B%7D%7D%7D&sortModel=%7B%22colId%22:%22Location%22,%22sort%22:%22asc%22%7D
 
-# Data is on line 580
+def run_abortion_policy_scraper():
+    """
+    This function runs web scraping for an abortion policy webpage.
+    """
+    url = "https://www.kff.org/womens-health-policy/state-indicator/gestational-limit-abortions/?currentTimeframe=0&selectedRows=%7B%22states%22:%7B%22all%22:%7B%7D%7D,%22wrapups%22:%7B%22united-states%22:%7B%7D%7D%7D&sortModel=%7B%22colId%22:%22Location%22,%22sort%22:%22asc%22%7D"
+    html = httpx.get(url)
+    html.raise_for_status()
+    root = lxml.html.fromstring(html.text)
 
-# div id="content" class="inner is-single-post"
-# script type="text/javascript"
+    rawdata_json = root.cssselect('#content script[type="text/javascript"]')[0].text
 
-# 577 si where the div starts, 668 is where this div ends
+    rawdata_json_clean = rawdata_json[57 : len(rawdata_json) - 3]
 
-# Look in gdocsObject
+    rawdata_dict = json.loads(rawdata_json_clean)
 
-url = "https://www.kff.org/womens-health-policy/state-indicator/gestational-limit-abortions/?currentTimeframe=0&selectedRows=%7B%22states%22:%7B%22all%22:%7B%7D%7D,%22wrapups%22:%7B%22united-states%22:%7B%7D%7D%7D&sortModel=%7B%22colId%22:%22Location%22,%22sort%22:%22asc%22%7D"
-html = httpx.get(url)
-html.raise_for_status()
-root = lxml.html.fromstring(html.text)
+    rawdata_list = rawdata_dict["gdocsObject"]
 
-rawdata_json = root.cssselect('#content script[type="text/javascript"]')[0].text
+    state_data = []
 
-#to_delete = r"	var appJs = appJs || {};    appJs = jQuery.extend(appJs, " #59
-#rawdata_json_clean = re.sub(to_delete, "", rawdata_json[:len(rawdata_json)-3])
-rawdata_json_clean =  rawdata_json[57:len(rawdata_json)-3]
+    for state_info in rawdata_list[0][1]:
+        if (
+            state_info[0] == "District of Columbia"
+            or state_info[0] == "United States"
+            or state_info[0] == ""
+        ):
+            continue
 
-rawdata_dict = json.loads(rawdata_json_clean)
+        state = state_info[0]
 
-rawdata_list = rawdata_dict["gdocsObject"]
+        if len(state_info) < 2:
+            limit = "NA"
+            exception = "NA"
+            legal = "NA"
+        elif len(state_info) < 4:
+            exception = "NA"
+            legal = "NA"
+        else:
+            limit = state_info[1]
+            exception = state_info[2]
+            legal = state_info[3]
 
-state_data = []
+        if state in STATE_ABBREVIATIONS:
+            abbrev = STATE_ABBREVIATIONS[state]
 
-for state_info in rawdata_list[0][1]:
-    if state_info[0] == 'District of Columbia' or state_info[0] == 'United States' or state_info[0] == "":
-        continue
+        state_data.append(
+            {
+                "Location": state,
+                "Abbreviation": abbrev,
+                "Statutory Limit on Abortions": limit,
+                "Exceptions to Statutory Limits on Abortions": exception,
+                "Legal Standard for Health/Life Exception": legal,
+            }
+        )
 
-    state = state_info[0]
+    field_names = [
+        "Location",
+        "Abbreviation",
+        "Statutory Limit on Abortions",
+        "Exceptions to Statutory Limits on Abortions",
+        "Legal Standard for Health/Life Exception",
+    ]
 
-    if len(state_info) < 2:
-        limit = "NA"
-        exception = "NA"
-        legal = "NA"
-    elif len(state_info) < 4:
-        exception = "NA"
-        legal = "NA"
-    else:
-        limit = state_info[1]
-        exception = state_info[2]
-        legal = state_info[3]
+    with open(BASE_DIR / "data/scrape_data/abortion.csv", "w") as file:
+        writer = csv.DictWriter(file, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(state_data[:])
 
-    if state in STATE_ABBREVIATIONS:
-        abbrev = STATE_ABBREVIATIONS[state]
 
-    state_data.append({'Location': state,
-                       'Abbreviation': abbrev,
-                       'Statutory Limit on Abortions': limit,
-                       'Exceptions to Statutory Limits on Abortions': exception,
-                       'Legal Standard for Health/Life Exception': legal})
-
-field_names = ['Location', 'Abbreviation', 'Statutory Limit on Abortions', 'Exceptions to Statutory Limits on Abortions', 'Legal Standard for Health/Life Exception']
-
-with open(BASE_DIR / 'data/scrape_data/abortion.csv', 'w') as file: 
-    writer = csv.DictWriter(file, fieldnames = field_names) 
-    writer.writeheader()
-    writer.writerows(state_data[:])
+run_abortion_policy_scraper()
--- mortality/scrapers/web_scraping_functions.py
+++ mortality/scrapers/web_scraping_functions.py
@@ -2,17 +2,21 @@ import json
 import csv
 import httpx
 from pathlib import Path
+from .kff_data_sources import DATA_SOURCES
+
 
 BASE_DIR = Path(__file__).parent.parent.parent
 
+
 def get_json_from_html(url: str) -> dict:
     """
-    This function takes in a url and returns the html, when the html is a json, 
-    and returns a python dictionary.
+    This function takes in a url and returns the html, when the html is a json,
+    and returns a python dictionary. This function is meant to be used with the
+    KFF data contained in kff_data_sources.py.
 
     Parameters:
         url: The url to the website that contains the json.
-    
+
     Returns:
         A dictionary with the data contained in the json.
     """
@@ -27,14 +31,15 @@ def extract_state_info(raw_data: list, variables: list, output_file: str):
     """
     This function parses a list of lists containing state data and writes the
     important information to a csv. Note that the length of the variables must
-    match the length of each list containing state information.
+    match the length of each list containing state information. This function is
+    meant to be used with the KFF data contained in kff_data_sources.py.
 
-    Parameters: 
+    Parameters:
         raw_data: a list of lists containing state data.
-        variables: a list of variable names, corresponding to the number of 
+        variables: a list of variable names, corresponding to the number of
             fields within each row of state data.
         output_file: the path and filename to where the data should be saved.
-    
+
     Returns:
         None, the information gets written to a file.
     """
@@ -47,7 +52,23 @@ def extract_state_info(raw_data: list, variables: list, output_file: str):
 
         data.append(row)
 
-    with open(BASE_DIR / output_file, 'w') as file:
-        writer = csv.DictWriter(file, fieldnames = variables)
+    with open(BASE_DIR / output_file, "w") as file:
+        writer = csv.DictWriter(file, fieldnames=variables)
         writer.writeheader()
         writer.writerows(data)
+
+
+def run_kff_scrapers(data_sources: dict):
+    """
+    This function runs the web scraping functions on the data to be scraped.
+
+    Parameters:
+        data_sources: The dictionary that contains all of the necessary
+            information needed to scrape all of the data sources.
+    """
+    for url, variables, start_index, output_file in data_sources.values():
+        info = get_json_from_html(url)
+        extract_state_info(info["data"][start_index:], variables, output_file)
+
+
+run_kff_scrapers(DATA_SOURCES)
