7733b968224526cebc2a4ea9d2b5b3e72358a533
Elizabeth Ronan <eronan@uchicago.edu>
2025-03-09 17:24:45 -0500

    Adding command line interface

--- mortality/__main__.py
+++ mortality/__main__.py
@@ -1,5 +1,6 @@
 import sys
-from .predict_model import user_input_dash
+from mortality.predict_model import user_input_dash
+from mortality.map_viz import run_app
 
 def main():
     if len(sys.argv) != 2:
@@ -8,6 +9,9 @@ def main():
     elif sys.argv[1] == "prediction":
         print("To close the Dash, please press Control+c (MacOs) or press Ctrl (Window)")
         user_input_dash()
+    elif sys.argv[1] == "map":
+        print("To close the Dash, please press Control+c (MacOs) or press Ctrl (Window)")
+        run_app()
 
 if __name__ == "__main__":
     main()
